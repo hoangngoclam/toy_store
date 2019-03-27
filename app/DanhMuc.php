b250d2314c8488130d9616776bc1430095215115
lam5propropro <lamthn.labianlabs@gmail.com>
2019-03-27 22:19:56 +0700

    update relationsip of model

--- app/DanhMuc.php
+++ app/DanhMuc.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class DanhMuc extends Model
 {
     protected $table = 'danh_muc';
+
+    public function loaisp()
+    {
+        return $this->hasMany('App\LoaiSP', 'id_danh_muc');
+    }
 }
--- app/KhachHang.php
+++ app/KhachHang.php
@@ -7,4 +7,12 @@ use Illuminate\Database\Eloquent\Model;
 class KhachHang extends Model
 {
     protected $table = 'khach_hang';
+    public function dssp()
+    {
+        return $this->hasMany('App\DanhSachSP', 'id_kh');
+    }
+    public function hoadon()
+    {
+        return $this->hasMany('App\HoaDon', 'id_kh');
+    }
 }
