80c49d506e62514dd48d28d8a1e472cc45534098
hanhnvctk40github <hanhnvctk40@gmail.com>
2019-03-29 08:56:43 +0700

    update giao dien trang chu

--- resources/views/trangChu.blade.php
+++ resources/views/trangChu.blade.php
@@ -1,16 +1,16 @@
 @extends('layout.index')
 @section('css')
-    <link rel="stylesheet" href="./css/trangChu.css">
+<link rel="stylesheet" href="./css/trangChu.css">
 @endsection
 
 @section('content')
 
 <div class="container-fluid mt-4">
-        <div class="row row_magin">
+    <div class="row row_magin">
 
-            <div class="card col-xs-4 col-sm-4 col-md-3 col-lg-2 col-xm-2 card_x ">
-                <div class="w-100 card-img-top card_img_top"
-                    style="background-image: url('images/1.jpg');">
+        <div class="col-xs-4 col-sm-4 col-md-3 col-lg-2 col-xm-2 card_x px-1">
+            <div class="card ">
+                <div class="w-100 card-img-top card_img_top" style="background-image: url('images/1.jpg');">
                 </div>
                 <div class="card-body">
                     <p class="card-title mb-0">Máy bay trực thăng di chuyển phát nhạc</p>
@@ -19,11 +19,13 @@
                     <span class="card_body_span2">-44%</span>
 
                     <a href="#" class="btn btn-primary btn-sm card_body_a">Thêm vào <i
-                            class="fas fa-cart-plus mr-2" ></i></a>
+                            class="fas fa-cart-plus mr-2"></i></a>
                 </div>
             </div>
+        </div>
 
-            <div class="card col-xs-4 col-sm-4 col-md-3 col-lg-2 col-xm-2 card_x" >
+        <div class="col-xs-4 col-sm-4 col-md-3 col-lg-2 col-xm-2 card_x px-1">
+            <div class="card ">
                 <div class="w-100 card-img-top"
                     style="background-image: url('images/2.jpg'); height: 200px; background-position: center;background-size:cover;">
                 </div>
@@ -34,11 +36,13 @@
                     <span class="card_body_span2">-44%</span>
 
                     <a href="#" class="btn btn-primary btn-sm card_body_a">Thêm vào <i
-                            class="fas fa-cart-plus mr-2" ></i></a>
+                            class="fas fa-cart-plus mr-2"></i></a>
                 </div>
             </div>
-            
-            <div class="card col-xs-4 col-sm-4 col-md-3 col-lg-2 col-xm-2 card_x" >
+        </div>
+
+        <div class=" col-xs-4 col-sm-4 col-md-3 col-lg-2 col-xm-2 card_x px-1">
+            <div class="card ">
                 <div class="w-100 card-img-top"
                     style="background-image: url('images/3.jpg'); height: 200px; background-position: center;background-size:cover;">
                 </div>
@@ -49,54 +53,55 @@
                     <span class="card_body_span2">-44%</span>
 
                     <a href="#" class="btn btn-primary btn-sm card_body_a">Thêm vào <i
-                            class="fas fa-cart-plus mr-2" ></i></a>
-                </div>
-            </div>
-            
-            <div class="card col-xs-4 col-sm-4 col-md-3 col-lg-2 col-xm-2 card_x" >
-                <div class="w-100 card-img-top"
-                    style="background-image: url('images/4.jpg'); height: 200px; background-position: center;background-size:cover;">
-                </div>
-                <div class="card-body">
-                    <p class="card-title mb-0">Máy bay trực thăng di chuyển phát nhạc</p>
-                    <h6 class="card_body_h6">1.150.000₫</h6>
-                    <span class="card_body_span1">2.000.000₫</span>
-                    <span class="card_body_span2">-44%</span>
-
-                    <a href="#" class="btn btn-primary btn-sm card_body_a">Thêm vào <i
-                            class="fas fa-cart-plus mr-2" ></i></a>
-                </div>
-            </div>
-            
-            <div class="card col-xs-4 col-sm-4 col-md-3 col-lg-2 col-xm-2 card_x" >
-                <div class="w-100 card-img-top"
-                    style="background-image: url('images/5.jpg'); height: 200px; background-position: center;background-size:cover;">
-                </div>
-                <div class="card-body">
-                    <p class="card-title mb-0">Máy bay trực thăng di chuyển phát nhạc</p>
-                    <h6 class="card_body_h6">1.150.000₫</h6>
-                    <span class="card_body_span1">2.000.000₫</span>
-                    <span class="card_body_span2">-44%</span>
-
-                    <a href="#" class="btn btn-primary btn-sm card_body_a">Thêm vào <i
-                            class="fas fa-cart-plus mr-2" ></i></a>
-                </div>
-            </div>
-            
-            <div class="card col-xs-4 col-sm-4 col-md-3 col-lg-2 col-xm-2 card_x" >
-                <div class="w-100 card-img-top"
-                    style="background-image: url('images/7.jpg'); height: 200px; background-position: center;background-size:cover;">
-                </div>
-                <div class="card-body">
-                    <p class="card-title mb-0">Máy bay trực thăng di chuyển phát nhạc</p>
-                    <h6 class="card_body_h6">1.150.000₫</h6>
-                    <span class="card_body_span1">2.000.000₫</span>
-                    <span class="card_body_span2">-44%</span>
-
-                    <a href="#" class="btn btn-primary btn-sm card_body_a">Thêm vào <i
-                            class="fas fa-cart-plus mr-2" ></i></a>
+                            class="fas fa-cart-plus mr-2"></i></a>
                 </div>
             </div>
         </div>
+
+        <div class="col-xs-4 col-sm-4 col-md-3 col-lg-2 col-xm-2 card_x">
+            <div class="w-100 card-img-top"
+                style="background-image: url('images/4.jpg'); height: 200px; background-position: center;background-size:cover;">
+            </div>
+            <div class="card-body">
+                <p class="card-title mb-0">Máy bay trực thăng di chuyển phát nhạc</p>
+                <h6 class="card_body_h6">1.150.000₫</h6>
+                <span class="card_body_span1">2.000.000₫</span>
+                <span class="card_body_span2">-44%</span>
+
+                <a href="#" class="btn btn-primary btn-sm card_body_a">Thêm vào <i
+                        class="fas fa-cart-plus mr-2"></i></a>
+            </div>
+        </div>
+
+        <div class="col-xs-4 col-sm-4 col-md-3 col-lg-2 col-xm-2 card_x">
+            <div class="w-100 card-img-top"
+                style="background-image: url('images/5.jpg'); height: 200px; background-position: center;background-size:cover;">
+            </div>
+            <div class="card-body">
+                <p class="card-title mb-0">Máy bay trực thăng di chuyển phát nhạc</p>
+                <h6 class="card_body_h6">1.150.000₫</h6>
+                <span class="card_body_span1">2.000.000₫</span>
+                <span class="card_body_span2">-44%</span>
+
+                <a href="#" class="btn btn-primary btn-sm card_body_a">Thêm vào <i
+                        class="fas fa-cart-plus mr-2"></i></a>
+            </div>
+        </div>
+
+        <div class="col-xs-4 col-sm-4 col-md-3 col-lg-2 col-xm-2 card_x">
+            <div class="w-100 card-img-top"
+                style="background-image: url('images/7.jpg'); height: 200px; background-position: center;background-size:cover;">
+            </div>
+            <div class="card-body">
+                <p class="card-title mb-0">Máy bay trực thăng di chuyển phát nhạc</p>
+                <h6 class="card_body_h6">1.150.000₫</h6>
+                <span class="card_body_span1">2.000.000₫</span>
+                <span class="card_body_span2">-44%</span>
+
+                <a href="#" class="btn btn-primary btn-sm card_body_a">Thêm vào <i
+                        class="fas fa-cart-plus mr-2"></i></a>
+            </div>
+        </div>
+    </div>
 </div>
 @endsection
